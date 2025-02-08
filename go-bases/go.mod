07027181c036c2cfce6b3a097bee485c299b97dd
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-08 17:59:59 -0500

    variables

---
+++ go-bases/go.mod
@@ -0,0 +1,3 @@
+module github.com/nelsonmarro/go-bases
+
+go 1.23.6
---
+++ go-bases/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var edad int = 18
+	var peso float64 = 72.123123123
+	// booleanos true o false
+	var activo bool = true
+	// para representar texto
+	var saludo string = "Hola, mundo"
+
+	// forma corta de declarar variables y asignarles un valor
+	altura := 1.80
+
+	fmt.Println("Tu edad es:", edad)
+	fmt.Println("el peso es:", peso)
+	fmt.Println("activo:", activo)
+	fmt.Println("saludo:", saludo)
+}
