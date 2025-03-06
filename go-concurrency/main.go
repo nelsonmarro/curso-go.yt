be1dc1cff4206d150cdb85bb1d3bdcc9d959161a
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-05 21:10:07 -0500

    /workspace/github/nelsonmarro/curso-go.yt

--- go-concurrency/main.go
+++ go-concurrency/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	sumaconcurrente "github.com/nelsonmarro/go-concurrency/suma_concurrente"
 )
 
-func saludar(nombre string) {
-	fmt.Printf("Hola, %s desde una goroutine!\n", nombre)
-}
+// func saludar(nombre string) {
+// 	fmt.Printf("Hola, %s desde una goroutine!\n", nombre)
+// }
 
 func main() {
-	go saludar("Gophers!")
-	fmt.Println("Hola desde la funcion principal!")
-	time.Sleep(1 * time.Second)
+	// go saludar("Gophers!")
+	// fmt.Println("Hola desde la funcion principal!")
+	// time.Sleep(1 * time.Second)
+	numeros1 := []int{1, 2, 3, 4, 5}
+	numeros2 := []int{6, 7, 8, 9, 10}
+
+	canalSuma := make(chan int)
+
+	go sumaconcurrente.Sumar(numeros1, canalSuma)
+	go sumaconcurrente.Sumar(numeros2, canalSuma)
+
+	suma1 := <-canalSuma
+	suma2 := <-canalSuma
+	fmt.Printf("Suma 1: %d, Suma 2: %d, Suma Total: %d\n", suma1, suma2, suma1+suma2)
 }
--- go-concurrency/suma_concurrente/suma_concurrente.go
+++ go-concurrency/suma_concurrente/suma_concurrente.go
@@ -1,1 +1,9 @@
 package sumaconcurrente
+
+func Sumar(numeros []int, canalResults chan int) {
+	suma := 0
+	for _, num := range numeros {
+		suma += num
+	}
+	canalResults <- suma
+}
