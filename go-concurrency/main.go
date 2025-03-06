4721b53494d1113707d03cee8855d4fcc7e5e397
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-05 21:00:07 -0500

    /workspace/github/nelsonmarro/curso-go.yt

--- go-concurrency/main.go
+++ go-concurrency/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func saludar(nombre string) {
+	fmt.Printf("Hola, %s desde una goroutine!\n", nombre)
+}
+
 func main() {
+	go saludar("Gophers!")
+	fmt.Println("Hola desde la funcion principal!")
+	time.Sleep(1 * time.Second)
 }
