67da1de65eb5c7360e3cb46c1f98a1dac593908a
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-07 18:30:06 -0500

    /workspace/github/nelsonmarro/curso-go.yt

--- go-concurrency/main.go
+++ go-concurrency/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func saludar(nombre string) {
+	fmt.Printf("Hola, %s desde una goroutine!\n", nombre)
+}
+
 func main() {
+	// invocar una funcion en una goroutine (de manera concurrente)
+	go saludar("CodeCrafter")
+	fmt.Println("Ejecutando en la goroutine principal...")
+	time.Sleep(1 * time.Second)
 }
