2f19dff8d3f682f79cd5d2c4526095016cd2d5ad
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-10 18:50:04 -0500

    /workspace/github/nelsonmarro/curso-go.yt

--- go-files/main.go
+++ go-files/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	rutaArchivo := "notas.txt"
+	archivoNotas, err := os.Open(rutaArchivo)
+	if err != nil {
+		fmt.Println("Error al abrir el archivo:", err)
+	}
+	defer archivoNotas.Close()
+
+	fmt.Printf("Contenido de '%s':\n", rutaArchivo)
+	escaner := bufio.NewScanner(archivoNotas)
+
+	// Leer línea a línea
+	for escaner.Scan() {
+		linea := escaner.Text()
+		fmt.Println(linea)
+	}
 }
