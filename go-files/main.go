542744cfb86c9ff0cad09868c74c530e0b108722
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-10 19:00:07 -0500

    /workspace/github/nelsonmarro/curso-go.yt

--- go-files/main.go
+++ go-files/main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
 
+// Leer un archivo existente y mostrar su contenido
+// func main() {
+// 	rutaArchivo := "notas.txt"
+// 	archivoNotas, err := os.Open(rutaArchivo)
+// 	if err != nil {
+// 		fmt.Println("Error al abrir el archivo:", err)
+// 	}
+// 	defer archivoNotas.Close()
+//
+// 	fmt.Printf("Contenido de '%s':\n", rutaArchivo)
+// 	escaner := bufio.NewScanner(archivoNotas)
+//
+// 	// Leer línea a línea
+// 	for escaner.Scan() {
+// 		linea := escaner.Text()
+// 		fmt.Printf("%s\n", linea)
+// 	}
+//
+// 	if err := escaner.Err(); err != nil {
+// 		fmt.Println("Error al leer el archivo:", err)
+// 	}
+// }
+
 func main() {
-	rutaArchivo := "notas.txt"
-	archivoNotas, err := os.Open(rutaArchivo)
+	nombreArchivo := "mi-archivo.txt"
+
+	miarchivo, err := os.Create(nombreArchivo)
 	if err != nil {
-		fmt.Println("Error al abrir el archivo:", err)
+		fmt.Println("Error al crear el archivo:", err)
+		return
 	}
-	defer archivoNotas.Close()
 
-	fmt.Printf("Contenido de '%s':\n", rutaArchivo)
-	escaner := bufio.NewScanner(archivoNotas)
+	textoInicial := "Texto inicial simplificado.\nLínea uno.\nLínea dos.\n"
+	fmt.Printf("Escribiendo texto inicial en '%s':\n%s", nombreArchivo, textoInicial)
 
-	// Leer línea a línea
-	for escaner.Scan() {
-		linea := escaner.Text()
-		fmt.Println(linea)
+	_, err = miarchivo.WriteString(textoInicial)
+	if err != nil {
+		fmt.Println("Error al escribir el texto:", err)
 	}
 }
